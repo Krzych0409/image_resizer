d664bde48cf81367d1dcfa7ae52f6ae4ba90d28c
Krzych0409 <krzychmich@onet.pl>
2023-01-24 19:23:46 +0100

    Add button 'Select images' and do function 'select_images'

--- resizer.py
+++ resizer.py
@@ -1,9 +1,32 @@
 import tkinter as tk
 import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from tkinter import filedialog as fd
+
+
+def select_images(extension_img: tuple[str]=('png', 'jpg', 'jpeg')):
+    # Open files window and select images
+    images_name = fd.askopenfilenames(initialdir='./', filetypes=(('All files', '*.*'), ('PNG image', '*.png'), ('JPG image', '*.jp*g')))
+    print(type(images_name))
+    # Leave only name file with extension
+    images_name = [name.lower().split('/')[-1] for name in images_name]
+    print(images_name)
+    # Delete all files without correct extension
+    images_name = [name for name in images_name if name.split('.')[-1] in extension_img]
+    print(images_name)
+    return images_name
+
+
 
 root = tb.Window(themename='superhero')
 root.title('Window image resizer')
 root.geometry('1000x500')
+
+
+button_open_dir = tb.Button(root, bootstyle=INFO, text='Select images', command=select_images)
+button_open_dir.pack()
+
+
 
 
 root.mainloop()
