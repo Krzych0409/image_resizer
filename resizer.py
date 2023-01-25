2f832ae338130463fb17d86cd051306677b89fac
Krzych0409 <krzychmich@onet.pl>
2023-01-26 00:09:34 +0100

    Finish frontend entire second Labelframe
    Add all widgets in second Labelframe. Choice one resize method.
    
    Issue: Resolve #8

--- resizer.py
+++ resizer.py
@@ -19,18 +19,22 @@ def select_images(extension_img: tuple[str]=('png', 'jpg', 'jpeg')):
     only_name_images = [name.split('/')[-1] for name in images_name]
     print(images_name)
 
-    add_images_to_listbox(only_name_images)
+    add_images_to_listbox(only_name_images, images_name)
 
+def add_images_to_listbox(only_name_images, images_name):
+    for img, path in zip(only_name_images, images_name):
+        size = str(int(os.stat(path).st_size / 1024)) + ' KB'
+        tree_img.insert('', END, values=(img, size))
+
+def clear_img_treeview():
+    for item in tree_img.get_children():
+        tree_img.delete(item)
 
 def select_new_dir():
     global new_dir_name
     new_dir_name.set(fd.askdirectory(title="Where to paste resized images? Select folder:"))
-    print(new_dir_name.get())
-
-def add_images_to_listbox(only_name_images):
-    for img in only_name_images:
-        size = str(int(os.stat(img).st_size / 1024)) + ' KB'
-        tree_img.insert('', END, values=(img, size))
+    print(choice_method.get())
+    print(meter_resize.amountusedvar.get())
 
 def get_all_image_name():
     global name_img_list
@@ -39,9 +43,50 @@ def get_all_image_name():
 
     return name_img_list
 
-def clear_img_treeview():
-    for item in tree_img.get_children():
-        tree_img.delete(item)
+def choice_resize_method():
+    if choice_method.get() == '%':
+        # Disable other resize method
+        entry_max_px.delete(0, len(entry_max_px.get()))
+        entry_max_px.configure(state=DISABLED)
+        entry_precise_x.delete(0, len(entry_precise_x.get()))
+        entry_precise_x.configure(state=DISABLED)
+        entry_precise_y.delete(0, len(entry_precise_y.get()))
+        entry_precise_y.configure(state=DISABLED)
+        # Enable this method
+        meter_resize.configure(interactive=True)
+
+    elif choice_method.get() == 'max_px':
+        # Disable other resize method
+        meter_resize.configure(amountused=0)
+        meter_resize.configure(interactive=False)
+        entry_precise_x.delete(0, len(entry_precise_x.get()))
+        entry_precise_x.configure(state=DISABLED)
+        entry_precise_y.delete(0, len(entry_precise_y.get()))
+        entry_precise_y.configure(state=DISABLED)
+        # Enable this method
+        entry_max_px.configure(state=NORMAL)
+
+    elif choice_method.get() == 'xy':
+        # Disable other resize method
+        meter_resize.configure(amountused=0)
+        meter_resize.configure(interactive=False)
+        entry_max_px.delete(0, len(entry_max_px.get()))
+        entry_max_px.configure(state=DISABLED)
+        # Enable this method
+        entry_precise_x.configure(state=NORMAL)
+        entry_precise_y.configure(state=NORMAL)
+
+
+
+    '''if choice_method.get() == 'meter':
+        global meter_resize
+        meter_resize = tb.Meter(labelframe_option, bootstyle=INFO, subtext='%', metersize=150, interactive=True, arcrange=180, arcoffset=180)
+        meter_resize.grid(row=1, column=0)
+    elif choice_method.get() == 'entry':
+        meter_resize.destroy()
+        entry = tb.Entry(labelframe_option, text=choice_method.get())
+        entry.grid(row=1, column=0)
+        print(choice_method.get())'''
 
 
 def resize_images(list_img_text: list[str], new_dir_name: str, max_px: int, des_ext: str='png'):
@@ -89,43 +134,99 @@ def resize_images(list_img_text: list[str], new_dir_name: str, max_px: int, des_
 
 root = tb.Window(themename='superhero')
 root.title('Window image resizer')
-root.geometry('1000x500')
+#root.geometry('1000x500')
 
 images_name = ''
 new_dir_name = tb.StringVar()
 name_img_list = []
 
 
+# Frame - select images
 labelframe_select = tb.Labelframe(root, bootstyle=INFO, text='Images to resize')
-labelframe_select.pack(anchor=NW, padx=10, pady=5)
+labelframe_select.grid(row=0, column=0, padx=10, pady=5, ipadx=0, ipady=0, sticky=N+S)
 
 button_select_images = tb.Button(labelframe_select, bootstyle=INFO+OUTLINE, text='Select images', command=select_images)
-button_select_images.grid(row=0, column=0, pady=10)
+button_select_images.grid(row=0, column=0, padx=5, pady=10, sticky=W)
 button_clear_tree = tb.Button(labelframe_select, bootstyle=INFO+OUTLINE, text='Clear list', command=clear_img_treeview)
-button_clear_tree.grid(row=0, column=1, columnspan=2, pady=10)
+button_clear_tree.grid(row=0, column=1, padx=5, pady=10, sticky=W)
 
 tree_img = tb.Treeview(labelframe_select, bootstyle=INFO, columns=('image', 'size'), show='headings')
 tree_img.heading('image', text='Image')
 tree_img.heading('size', text='Size')
 tree_img.column('image', width=250, minwidth=200)
-tree_img.column('size', width=60, minwidth=30)
+tree_img.column('size', width=60, minwidth=50)
 tree_img.grid(row=1, column=0, columnspan=2)
-select_scroll_x = tb.Scrollbar(labelframe_select, bootstyle=INFO, orient=HORIZONTAL, command=tree_img.xview)
-select_scroll_y = tb.Scrollbar(labelframe_select, bootstyle=INFO, orient=VERTICAL, command=tree_img.yview)
+select_scroll_x = tb.Scrollbar(labelframe_select, bootstyle=INFO+ROUND, orient=HORIZONTAL, command=tree_img.xview)
+select_scroll_y = tb.Scrollbar(labelframe_select, bootstyle=INFO+ROUND, orient=VERTICAL, command=tree_img.yview)
 tree_img.configure(xscrollcommand=select_scroll_x.set)
 tree_img.configure(yscrollcommand=select_scroll_y.set)
 select_scroll_x.grid(row=2, column=0, columnspan=2, sticky=W+E)
 select_scroll_y.grid(row=1, column=2, sticky=N+S)
+belt_select = tb.Panedwindow(labelframe_select, bootstyle=SECONDARY, orient=HORIZONTAL, height=5)
+belt_select.grid(row=3, column=0, columnspan=3, pady=20, sticky=W+E)
+
+button_select_new_dir = tb.Button(labelframe_select, bootstyle=INFO+OUTLINE, text='Select directory for new images', command=select_new_dir)
+button_select_new_dir.grid(row=4, column=0, columnspan=3, padx=5, pady=10, sticky=N)
+entry_select_new_dir = tb.Entry(labelframe_select, textvariable=new_dir_name)
+entry_select_new_dir.grid(row=5, column=0, columnspan=3, padx=5, sticky=W+E)
+new_dir_scroll_x = tb.Scrollbar(labelframe_select, bootstyle=INFO+ROUND, orient=HORIZONTAL, command=entry_select_new_dir.xview)
+new_dir_scroll_x.grid(row=6, column=0, columnspan=3, padx=5, sticky=W+E)
+entry_select_new_dir.configure(xscrollcommand=new_dir_scroll_x.set)
+
+# Frame - options
+labelframe_option = tb.Labelframe(root, bootstyle=INFO, text='Options')
+labelframe_option.grid(row=0, column=1, padx=10, pady=5, ipadx=0, ipady=5, sticky=N+S)
+
+choice_method = tb.StringVar()
+
+radiobutton_meter = tb.Radiobutton(labelframe_option, bootstyle=PRIMARY+TOOLBUTTON+OUTLINE, text='Percentage', variable=choice_method,
+                                   value='%', command=choice_resize_method)
+radiobutton_meter.grid(row=0, column=0, padx=10, pady=5)
+meter_value = tb.IntVar()
+meter_resize = tb.Meter(labelframe_option, bootstyle=PRIMARY, metersize=130, textright='%', arcrange=320, arcoffset=110,
+                        stripethickness=3, meterthickness=8)
+meter_resize.grid(row=1, column=0)
+belt_meter = tb.Panedwindow(labelframe_option, bootstyle=SECONDARY, orient=HORIZONTAL, height=3)
+belt_meter.grid(row=2, column=0, pady=5, sticky=W+E)
+
+radiobutton_px = tb.Radiobutton(labelframe_option, bootstyle=WARNING+TOOLBUTTON+OUTLINE, text='Longer side in px', variable=choice_method,
+                                value='max_px', command=choice_resize_method)
+radiobutton_px.grid(row=3, column=0, padx=5, pady=5)
+entry_max_px = tb.Entry(labelframe_option, bootstyle=WARNING, state=DISABLED)
+entry_max_px.grid(row=4, column=0, padx=5, pady=5)
+belt_max_px = tb.Panedwindow(labelframe_option, bootstyle=SECONDARY, orient=HORIZONTAL, height=3)
+belt_max_px.grid(row=5, column=0, pady=5, sticky=W+E)
+
+radiobutton_method = tb.Radiobutton(labelframe_option, bootstyle=SUCCESS+TOOLBUTTON+OUTLINE, text='Precise size', variable=choice_method,
+                                    value='xy', command=choice_resize_method)
+radiobutton_method.grid(row=6, column=0, padx=10, pady=5)
+frame_precise = tb.Frame(labelframe_option)
+frame_precise.grid(row=7, column=0)
+label_precise_x = tb.Label(frame_precise, bootstyle=SUCCESS, text='Width:')
+label_precise_x.grid(row=0, column=0, padx=5)
+entry_precise_x = tb.Entry(frame_precise, bootstyle=SUCCESS, state=DISABLED)
+entry_precise_x.grid(row=0, column=1, pady=5)
+label_px_x = tb.Label(frame_precise, bootstyle=SUCCESS, text='px')
+label_px_x.grid(row=0, column=2, padx=5)
 
 
-button_select_new_dir = tb.Button(labelframe_select, bootstyle=INFO+OUTLINE, text='Select directory', command=select_new_dir)
-button_select_new_dir.grid(row=3, column=0, padx=5, pady=10, sticky=W)
-label_select_new_dir = tb.Label(labelframe_select, textvariable=new_dir_name)
-label_select_new_dir.grid(row=3, column=1)
-
+label_precise_y = tb.Label(frame_precise, bootstyle=SUCCESS, text='Height:')
+label_precise_y.grid(row=1, column=0, padx=5)
+entry_precise_y = tb.Entry(frame_precise, bootstyle=SUCCESS, state=DISABLED)
+entry_precise_y.grid(row=1, column=1)
+label_px_y = tb.Label(frame_precise, bootstyle=SUCCESS, text='px')
+label_px_y.grid(row=1, column=2, padx=5)
 
 button_resize = tb.Button(root, bootstyle=INFO+OUTLINE, text='Resize all images !!!', command=lambda: resize_images(get_all_image_name(), new_dir_name.get(), 200))
-button_resize.pack(padx=30)
+button_resize.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+
+sizegrip_main = tb.Sizegrip(root, bootstyle=INFO).grid(row=1, column=20, sticky=SE)
+
+
+
+
+
+
 
 
 
