6b17872c96ea845e3f770020c1da1ffab95d1fae
Krzych0409 <krzychmich@onet.pl>
2023-01-26 13:44:42 +0100

    Rebuild resize function
    Issue: Resolve #9

--- resizer.py
+++ resizer.py
@@ -8,18 +8,24 @@ import os
 
 def select_images(extension_img: tuple[str]=('png', 'jpg', 'jpeg')):
     # Open files window and select images
-    global images_name
+    all_file_path = []
     images_name = fd.askopenfilenames(initialdir='./', filetypes=(('All files', '*.*'), ('PNG image', '*.png'), ('JPG image', '*.jp*g')))
-    print(images_name)
-    if not images_name:
+    all_file_path += images_name
+
+    print(f'All file path: {all_file_path}')
+
+    if not all_file_path:
         return 0
 
     # Delete all files without correct extension
-    images_name = [name for name in images_name if name.lower().split('.')[-1] in extension_img]
-    only_name_images = [name.split('/')[-1] for name in images_name]
-    print(images_name)
+    global images_path
+    images_path = [image_p for image_p in all_file_path if image_p.lower().split('.')[-1] in extension_img]
+    only_name_images = [name.split('/')[-1] for name in images_path]
 
-    add_images_to_listbox(only_name_images, images_name)
+    print(f'All images path: {images_path}')
+    print(f'All images name: {only_name_images}')
+
+    add_images_to_listbox(only_name_images, images_path)
 
 def add_images_to_listbox(only_name_images, images_name):
     for img, path in zip(only_name_images, images_name):
@@ -76,69 +82,117 @@ def choice_resize_method():
         entry_precise_x.configure(state=NORMAL)
         entry_precise_y.configure(state=NORMAL)
 
+def resize_images(method, list_img_text: list[str], new_dir_name: str='./', final_ext: str='same'):
+    # Do new directory if not exist
+    if not os.path.isdir(new_dir_name):
+        os.mkdir(new_dir_name)
 
 
-    '''if choice_method.get() == 'meter':
-        global meter_resize
-        meter_resize = tb.Meter(labelframe_option, bootstyle=INFO, subtext='%', metersize=150, interactive=True, arcrange=180, arcoffset=180)
-        meter_resize.grid(row=1, column=0)
-    elif choice_method.get() == 'entry':
-        meter_resize.destroy()
-        entry = tb.Entry(labelframe_option, text=choice_method.get())
-        entry.grid(row=1, column=0)
-        print(choice_method.get())'''
+    # If choice: percentage method - meter
+    if method == '%':
+        # Disable meter
+        meter_resize.configure(interactive=False)
+        percentage = meter_value.get()
 
+        for img_text in list_img_text:
+            # Load image with orientation
+            image = ImageOps.exif_transpose(Image.open(img_text))
+            width_img, height_img = image.size
 
-def resize_images(list_img_text: list[str], new_dir_name: str, max_px: int, des_ext: str='png'):
-    max_length_vertical = 0
-    max_length_horizontal = 0
-    for img_text in list_img_text:
-        # Load image with orientation
-        image = ImageOps.exif_transpose(Image.open(img_text))
+            # Count new values image X and Y in px
+            new_width_img = int(width_img * percentage / 100)
+            new_height_img = int(height_img * percentage / 100)
 
-        width_img, height_img = image.size
+            # Set final extension
+            if final_ext == 'same': extension = (img_text.split("/")[-1]).split(".")[-1]
+            else: extension = final_ext
 
-        # Do new directory
-        if not os.path.isdir(new_dir_name):
-            os.mkdir(new_dir_name)
-
-        # Check position image
-        if width_img > height_img:
-            scale = width_img / max_px
-            width_img_resize = max_px
-            height_img_resize = int(height_img / scale)
-            if height_img_resize > max_length_vertical:
-                max_length_vertical = height_img_resize
-            if width_img_resize > max_length_horizontal:
-                max_length_horizontal = width_img_resize
-
-            img_resize = image.resize((width_img_resize, height_img_resize))
+            # Resize image and save with final extension in select directory
+            img_resize = image.resize((new_width_img, new_height_img))
             new_image_name = (img_text.split("/")[-1]).split(".")[0]
-            print(new_image_name)
-            img_resize.save(f'{new_dir_name}/{new_image_name}.{des_ext}')
+            img_resize.save(f'{new_dir_name}/{new_image_name}.{extension}')
 
-        else:
-            scale = height_img / max_px
-            height_img_resize = max_px
-            width_img_resize = int(width_img / scale)
-            if height_img_resize > max_length_vertical:
-                max_length_vertical = height_img_resize
-            if width_img_resize > max_length_horizontal:
-                max_length_horizontal = width_img_resize
+            print(f'new image: {new_image_name}.{extension}')
 
-            img_resize = image.resize((width_img_resize, height_img_resize))
+    # If choice: longer side in px
+    elif method == 'max_px':
+        # Disable entry widget
+        entry_max_px.configure(state=DISABLED)
+        max_px = entry_max_px.get()
+        max_length_vertical = 0
+        max_length_horizontal = 0
+        for img_text in list_img_text:
+            # Set final extension
+            if final_ext == 'same':
+                extension = (img_text.split("/")[-1]).split(".")[-1]
+            else:
+                extension = final_ext
+
+            # Load image with orientation
+            image = ImageOps.exif_transpose(Image.open(img_text))
+            width_img, height_img = image.size
+
+            # Check position image
+            if width_img > height_img:
+                scale = width_img / max_px
+                width_img_resize = max_px
+                height_img_resize = int(height_img / scale)
+                if height_img_resize > max_length_vertical:
+                    max_length_vertical = height_img_resize
+                if width_img_resize > max_length_horizontal:
+                    max_length_horizontal = width_img_resize
+
+                img_resize = image.resize((width_img_resize, height_img_resize))
+                new_image_name = (img_text.split("/")[-1]).split(".")[0]
+                img_resize.save(f'{new_dir_name}/{new_image_name}.{extension}')
+
+            else:
+                scale = height_img / max_px
+                height_img_resize = max_px
+                width_img_resize = int(width_img / scale)
+                if height_img_resize > max_length_vertical:
+                    max_length_vertical = height_img_resize
+                if width_img_resize > max_length_horizontal:
+                    max_length_horizontal = width_img_resize
+
+                img_resize = image.resize((width_img_resize, height_img_resize))
+                new_image_name = (img_text.split("/")[-1]).split(".")[0]
+                img_resize.save(f'{new_dir_name}/{new_image_name}.{extension}')
+
+            print(f'new image: {new_image_name}.{extension}')
+
+    # If choise: precise size in px
+    elif method == 'xy':
+        # Disable 2 entry widgets
+        entry_precise_x.configure(state=DISABLED)
+        entry_precise_y.configure(state=DISABLED)
+        new_width_img = entry_precise_x.get()
+        new_height_img = entry_precise_y.get()
+
+        for img_text in list_img_text:
+            # Load image with orientation
+            image = ImageOps.exif_transpose(Image.open(img_text))
+
+            # Set final extension
+            if final_ext == 'same': extension = (img_text.split("/")[-1]).split(".")[-1]
+            else: extension = final_ext
+
+            # Resize image and save with final extension in select directory
+            img_resize = image.resize((new_width_img, new_height_img))
             new_image_name = (img_text.split("/")[-1]).split(".")[0]
-            print(new_image_name)
-            img_resize.save(f'{new_dir_name}/{new_image_name}.{des_ext}')
+            img_resize.save(f'{new_dir_name}/{new_image_name}.{extension}')
+
+            print(f'new image: {new_image_name}.{extension}')
 
 
 root = tb.Window(themename='superhero')
 root.title('Window image resizer')
 #root.geometry('1000x500')
 
-images_name = ''
+
 new_dir_name = tb.StringVar()
-name_img_list = []
+choice_method = tb.StringVar()
+meter_value = tb.IntVar()
 
 
 # Frame - select images
@@ -173,16 +227,16 @@ new_dir_scroll_x = tb.Scrollbar(labelframe_select, bootstyle=INFO+ROUND, orient=
 new_dir_scroll_x.grid(row=6, column=0, columnspan=3, padx=5, sticky=W+E)
 entry_select_new_dir.configure(xscrollcommand=new_dir_scroll_x.set)
 
+
 # Frame - options
 labelframe_option = tb.Labelframe(root, bootstyle=INFO, text='Options')
 labelframe_option.grid(row=0, column=1, padx=10, pady=5, ipadx=0, ipady=5, sticky=N+S)
 
-choice_method = tb.StringVar()
 
 radiobutton_meter = tb.Radiobutton(labelframe_option, bootstyle=PRIMARY+TOOLBUTTON+OUTLINE, text='Percentage', variable=choice_method,
                                    value='%', command=choice_resize_method)
 radiobutton_meter.grid(row=0, column=0, padx=10, pady=5)
-meter_value = tb.IntVar()
+
 meter_resize = tb.Meter(labelframe_option, bootstyle=PRIMARY, metersize=130, textright='%', arcrange=320, arcoffset=110,
                         stripethickness=3, meterthickness=8)
 meter_resize.grid(row=1, column=0)
@@ -209,7 +263,6 @@ entry_precise_x.grid(row=0, column=1, pady=5)
 label_px_x = tb.Label(frame_precise, bootstyle=SUCCESS, text='px')
 label_px_x.grid(row=0, column=2, padx=5)
 
-
 label_precise_y = tb.Label(frame_precise, bootstyle=SUCCESS, text='Height:')
 label_precise_y.grid(row=1, column=0, padx=5)
 entry_precise_y = tb.Entry(frame_precise, bootstyle=SUCCESS, state=DISABLED)
@@ -217,12 +270,24 @@ entry_precise_y.grid(row=1, column=1)
 label_px_y = tb.Label(frame_precise, bootstyle=SUCCESS, text='px')
 label_px_y.grid(row=1, column=2, padx=5)
 
-button_resize = tb.Button(root, bootstyle=INFO+OUTLINE, text='Resize all images !!!', command=lambda: resize_images(get_all_image_name(), new_dir_name.get(), 200))
-button_resize.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
-sizegrip_main = tb.Sizegrip(root, bootstyle=INFO).grid(row=1, column=20, sticky=SE)
+# Frame - resize
+labelframe_resize = tb.Labelframe(root, bootstyle=INFO, text='Resize')
+labelframe_resize.grid(row=0, column=2, padx=10, pady=5, ipadx=0, ipady=5, sticky=N+S)
 
 
+frame_bottom = tb.Labelframe(root, bootstyle=INFO, text='Resize')
+frame_bottom.grid(row=1, column=0)
+
+progressbar = tb.Progressbar(root, bootstyle=DANGER+STRIPED)
+progressbar.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky=W+E)
+progressbar.configure(value=50)
+
+button_resize = tb.Button(labelframe_resize, bootstyle=INFO+OUTLINE, text='Resize all images !!!',
+                          command=lambda: resize_images(choice_method.get(), images_path, new_dir_name.get()))
+button_resize.grid(row=1, column=0, padx=10, pady=5)
+
+sizegrip_main = tb.Sizegrip(root, bootstyle=INFO).grid(row=1, column=5, sticky=SE)
 
 
 
